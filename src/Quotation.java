f4cecce2f13d2dd3491afecc2d1316fc19e868e9
FOUREX <76780880+FOUREX@users.noreply.github.com>
2024-01-19 14:07:04 +0000

    Yep

---
+++ src/Quotation.java
@@ -0,0 +1,9 @@
+public class Quotation {
+    String quote = "Працює - не трогай!";
+    String author = "Програмісти 21 ст.";
+
+    public void display() {
+        System.out.println(quote);
+        System.out.println("\t" + author);
+    }
+}
